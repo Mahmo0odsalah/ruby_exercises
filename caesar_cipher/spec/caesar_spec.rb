5886ebecbff4e1050599607a66bc4462bcee364e
Mahmood <medoysf@gmail.com>
2021-08-05 10:30:51 +0200

    added rspec testing for caesar cipher

--- caesar_cipher/spec/caesar_spec.rb
+++ caesar_cipher/spec/caesar_spec.rb
@@ -1,38 +1,27 @@
-# frozen_string_literal: true
+#spec/caesar_spec.rb
+require("./lib/caesar_cipher.rb")
 
-# spec/caesar_spec.rb
-require './lib/caesar_cipher.rb'
-
-describe CaesarCipher do
-  subject { CaesarCipher.new }
-
-  describe '#translate' do
-    it 'works with small positive shift' do
-      expect(subject.translate('Zz', 5)).to eql('Ee')
+describe CaesarCipher do 
+  my_caesar = CaesarCipher.new
+  describe "#translate" do
+    it "returns cipher of simple cipher operation" do
+      expect(my_caesar.translate("abc",1)).to eql("bcd")
     end
 
-    it 'works with small negative shift' do
-      expect(subject.translate('Ee', -5)).to eql('Zz')
+    it "returns cipher while perserving case" do
+      expect(my_caesar.translate("AbC",5)).to eql("FgH")
     end
 
-    it 'works with large positive shift' do
-      expect(subject.translate('Zz', 83)).to eql('Ee')
+    it "returns cipher and wraps around the alphabet" do
+      expect(my_caesar.translate("vyZ",4)).to eql("zcD")
     end
 
-    it 'works with large negative shift' do
-      expect(subject.translate('Ee', -83)).to eql('Zz')
+    it "returns cipher of letters and perserves special characters" do
+      expect(my_caesar.translate("$vyZ!",4)).to eql("$zcD!")
     end
 
-    it 'works with a phrase with punctuation' do
-      expect(subject.translate('What a string!', 5)).to eql('Bmfy f xywnsl!')
-    end
-
-    it 'works with a phrase with a large shift' do
-      expect(subject.translate('Hello, World!', 75)).to eql('Ebiil, Tloia!')
-    end
-
-    it 'works with a phrase with a large negative shift' do
-      expect(subject.translate('Hello, World!', -55)).to eql('Ebiil, Tloia!')
+    it "returns cipher of letters with a very large shift value" do
+      expect(my_caesar.translate("abc",31)).to eql("fgh")
     end
   end
 end
